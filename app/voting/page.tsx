8ce075885fe63c3ff5a9f5e470329e23fa928432
Hannes Schaletzky <hannes.schaletzky@digitalservice.bund.de>
2023-11-29 13:37:41 +0100

    add loading indicator on voting page

--- app/voting/page.tsx
+++ app/voting/page.tsx
@@ -1,32 +1,48 @@
 /* eslint-disable @next/next/no-img-element */
 "use client";
 
-import { randomUUID } from "crypto";
 import { useEffect, useState } from "react";
+import Spinner from "../components/spinner";
+
+const initialRemainingUpvotes = 3;
 
 export default function Voting() {
   const [entries, setEntries] = useState<Entry[]>([]);
-  const [loading, setLoading] = useState(false);
-  const [remainingUpvotes, setRemainingUpvotes] = useState(3);
+  const [loading, setLoading] = useState(true);
+  const [photosFetched, setPhotosFetched] = useState(0);
+  const [amountPhotos, setAmountPhotos] = useState(0);
+  const [remainingUpvotes, setRemainingUpvotes] = useState(
+    initialRemainingUpvotes
+  );
 
   useEffect(() => {
     const remainingUpvotes = localStorage.getItem("remainingUpvotes");
     if (remainingUpvotes) {
       setRemainingUpvotes(Number(remainingUpvotes));
     } else {
-      localStorage.setItem("remainingUpvotes", "3");
+      localStorage.setItem(
+        "remainingUpvotes",
+        initialRemainingUpvotes.toString()
+      );
     }
 
     fetch("/api/db")
       .then((res) => res.json())
       .then(async (body) => {
         const cachedEntries: Entry[] = Object.values(body);
+        setAmountPhotos(cachedEntries.length);
+        // fetch photos
         for (let i = 0; i < cachedEntries.length; i++) {
           const entry = cachedEntries[i];
           entry.base64img = await fetchPhoto(entry.filename);
+          setPhotosFetched(photosFetched + 1);
         }
+        // sort desc
+        cachedEntries.sort((a, b) => b.upvotes - a.upvotes);
+        // display
         console.log(cachedEntries);
         setEntries(cachedEntries);
+        setLoading(false);
       });
   }, []);
 
@@ -58,42 +74,57 @@ export default function Voting() {
   }
 
   return (
-    <div className="slider">
-      {entries.map((entry, i) => (
-        <section key={crypto.randomUUID()}>
-          <img src={entry.base64img} alt={entry.name} />
-          <div className="flex justify-around items-center px-8 py-2">
+    <>
+      {loading && (
+        <>
+          <div className="flex flex-col items-center justify-center w-screen h-screen gap-3">
+            <Spinner />
             <div>
-              <div className="text-md">{entry.name}</div>
-
-              <div className="font-semibold ">{entry.upvotes} 👍</div>
+              {photosFetched} of {amountPhotos} loaded...
             </div>
-
-            <div>
-              {remainingUpvotes == 0 && (
-                <button className="bg-blue-200 text-white font-bold py-2 px-4 rounded my-4">
-                  <s>Upvote!</s> <br />
-                  {remainingUpvotes} 👍 left
-                </button>
-              )}
-              {remainingUpvotes > 0 && (
-                <button
-                  onClick={() => {
-                    upvote(entry.name);
-                  }}
-                  className="bg-blue-500 text-white font-bold py-2 px-4 rounded my-4"
-                >
-                  Upvote! <br />
-                  {remainingUpvotes} 👍 left
-                </button>
-              )}
-            </div>
-            <div>
-              <b>{i + 1}</b> of <b>{entries.length}</b>
-            </div>
+            <div>Sorry for the wait 😇</div>
           </div>
-        </section>
-      ))}
-    </div>
+        </>
+      )}
+      {!loading && (
+        <div className="slider">
+          {entries.map((entry, i) => (
+            <section key={crypto.randomUUID()}>
+              <img src={entry.base64img} alt={entry.name} />
+              <div className="flex justify-around items-center px-8 py-2">
+                <div>
+                  <div className="text-md">{entry.name}</div>
+
+                  <div className="font-semibold ">{entry.upvotes} 👍</div>
+                </div>
+
+                <div>
+                  {remainingUpvotes == 0 && (
+                    <button className="bg-blue-200 text-white font-bold py-2 px-4 rounded my-4">
+                      <s>Upvote!</s> <br />
+                      {remainingUpvotes} 👍 left
+                    </button>
+                  )}
+                  {remainingUpvotes > 0 && (
+                    <button
+                      onClick={() => {
+                        upvote(entry.name);
+                      }}
+                      className="bg-blue-500 text-white font-bold py-2 px-4 rounded my-4"
+                    >
+                      Upvote! <br />
+                      {remainingUpvotes} 👍 left
+                    </button>
+                  )}
+                </div>
+                <div>
+                  <b>{i + 1}</b> of <b>{entries.length}</b>
+                </div>
+              </div>
+            </section>
+          ))}
+        </div>
+      )}
+    </>
   );
 }
